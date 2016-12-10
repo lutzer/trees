bb1fe36004b924ccb8d1f4d045167e52647e6cb8
Erich Grunewald <erichgrunewald@gmail.com>
2016-12-10 12:35:05 +0100

    Reindent.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,22 +5,22 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetVerticalSync(true);
-    
+
     // this uses depth information for occlusion
     // rather than always drawing things on top of each other
     ofEnableDepthTest();
-    
+
     // this sets the camera's distance from the object
     cam.setDistance(100);
-    
+
     // create ground mesh
     ground.setMode(OF_PRIMITIVE_LINE_LOOP);
     ground.addVertex(ofPoint(-GROUND_SIZE/2,-GROUND_SIZE/2,0));
     ground.addVertex(ofPoint(GROUND_SIZE/2,-GROUND_SIZE/2,0));
     ground.addVertex(ofPoint(GROUND_SIZE/2,GROUND_SIZE/2,0));
     ground.addVertex(ofPoint(-GROUND_SIZE/2,GROUND_SIZE/2,0));
-    
-    
+
+
     ofEnableDepthTest();
     glEnable(GL_POINT_SMOOTH); // use circular points instead of square points
     glPointSize(3);// make the points bigger
@@ -33,14 +33,14 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    
+
     ofBackgroundGradient(ofColor::gray, ofColor::black, OF_GRADIENT_CIRCULAR);
-    
+
     // even points can overlap with each other, let's avoid that
     cam.begin();
     ground.draw();
     cam.end();
-    
+
 }
 
 //--------------------------------------------------------------
@@ -94,6 +94,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
