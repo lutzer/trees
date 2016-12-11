37d44dc655e3c49731d152a4bc827c76e58f3ba0
Erich Grunewald <erichgrunewald@gmail.com>
2016-12-11 18:45:36 +0100

    Add phototropism file with empty data structure.

--- src/simulation/phototropism.cpp
+++ src/simulation/phototropism.cpp
@@ -7,3 +7,10 @@
 //
 
 #include "phototropism.hpp"
+
+static const int binsPerAxis = 16;
+
+struct LightBins {
+    float densities[binsPerAxis][binsPerAxis];
+    float light[binsPerAxis][binsPerAxis];
+};
