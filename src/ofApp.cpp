3ada635b8f09bcabe05d9ffc4f83e5603d018882
Erich Grunewald <erichgrunewald@gmail.com>
2016-12-11 23:21:03 +0100

    Fix bug in generating densities.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -62,7 +62,7 @@ void ofApp::setup(){
 void ofApp::update(){
     iterationSlider->update();
 
-    bins = gen::lightBinsFromTree(treeList[iteration], sun, {{-100,0},{100,0}});
+    bins = gen::lightBinsFromTree(treeList[iteration], sun, { { -100, 0 }, { 200, 100 } });
     BinMesh binVisualisation = BinMesh(bins.densities.data(), photo::binsPerAxis, photo::binsPerAxis);
     binMesh = binVisualisation.getMesh(ofPoint(-100,0,0), ofPoint(5,5,5));
 
--- src/simulation/phototropism.cpp
+++ src/simulation/phototropism.cpp
@@ -8,21 +8,22 @@
 
 #include "phototropism.hpp"
 
+#include <math.h>
+
 int photo::binIndexForPoint(pts::Point point, pts::BoundingBox boundingBox) {
-    auto binSizeX = boundingBox.size.width / (double)photo::binsPerAxis;
-    auto binSizeY = boundingBox.size.height / (double)photo::binsPerAxis;
-    auto x = (int)(point.x / binSizeX);
-    auto y = (int)(point.y / binSizeY);
+    const auto binSizeX = boundingBox.size.width / (double)photo::binsPerAxis;
+    const auto binSizeY = boundingBox.size.height / (double)photo::binsPerAxis;
+    const auto x = (int) floor((point.x - boundingBox.origin.x) / binSizeX);
+    const auto y = (int) floor((point.y - boundingBox.origin.y) / binSizeY);
 
     return x + y * photo::binsPerAxis;
 }
 
 photo::BinArray photo::combineBins(photo::BinArray bins1, photo::BinArray bins2) {
-    const int binCount = photo::binsPerAxis * photo::binsPerAxis;
     photo::BinArray result;
     result.fill(0.0);
 
-    for (int i = 0; i++; i < binCount) {
+    for (int i = 0; i < result.size(); i++) {
         result[i] = bins1[i] + bins2[i];
     }
 
